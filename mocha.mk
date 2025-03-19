016da5524d958aac952a484442bc605674386abc
apple <105837597+380583067@users.noreply.github.com>
2025-03-19 23:06:22 +0800

    Fix some errors

--- mocha.mk
+++ mocha.mk
@@ -350,7 +350,9 @@ PRODUCT_PACKAGES += \
 
 # Widevine DRM
 PRODUCT_PACKAGES += \
-    libprotobuf_shim \
+    libprotobuf_shim
+
+PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
 
 # Wifi
