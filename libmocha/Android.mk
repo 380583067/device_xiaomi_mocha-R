d29ec166241b82e6c864e02e6493b2e034622b0f
apple <105837597+380583067@users.noreply.github.com>
2025-04-05 20:09:03 +0800

    Add files via upload

--- libmocha/Android.mk
+++ libmocha/Android.mk
@@ -26,25 +26,29 @@ LOCAL_SRC_FILES:= \
         camera/CameraUtils.cpp \
         camera/VendorTagDescriptor.cpp \
         camera/CameraParameters.cpp \
-        #sensor.c \
+        sensor.c \
         camera.c
 
 
 LOCAL_SHARED_LIBRARIES := \
-        libcutils \
         libutils \
         liblog \
         libbinder \
         libhardware \
         libui \
         libgui \
-        libcamera_metadata
+        libcamera_metadata \
+        libhidltransport \
+        libsensor \
+	android.hidl.token@1.0-utils \
+	android.hardware.graphics.bufferqueue@1.0
 
 LOCAL_C_INCLUDES += \
         $(LOCAL_PATH)/camera/include \
         system/media/camera/include \
         system/media/private/camera/include
 
+LOCAL_HEADER_LIBRARIES := libnativebase_headers
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE := libmocha_camera
 LOCAL_MODULE_TAGS := optional
@@ -57,7 +61,6 @@ LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE := libmocha_omx
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
-
 
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := powerservice_client.c 
@@ -78,4 +81,3 @@ LOCAL_SANITIZE := never
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 include $(BUILD_SHARED_LIBRARY)
-
